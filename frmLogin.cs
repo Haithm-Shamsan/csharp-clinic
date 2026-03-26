aa9690706fbabb093e51fd1d4f6e662b1117b1ad
Haithm-Shamsan <Haithm-Shamsan@users.noreply.github.com>
2026-03-26 18:41:21 +0300

    Initial commit

---
+++ frmLogin.cs
@@ -0,0 +1,37 @@
+﻿using BussnisLayer_Clinic;
+using Clinic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataLayer_Clinic1
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+
+       
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = clsBussnisLogin.LoginData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            
+        }
+    }
+}
